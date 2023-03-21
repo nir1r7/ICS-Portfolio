bfb0fdf350b5650d490e8425ace36b99ccb1e132
Nirvan Rabbani <nir1r7@gmail.com>
2023-03-21 18:01:21 -0400

    added the rest of the files linked up all the navbar pages added some animations to the navbar dropdown worked on the navbar borders not sure whether to keep it circular or return it to a box

---
+++ navbar.js
@@ -0,0 +1,33 @@
+
+function navbar(){
+    return (
+        `<div class="navbar">
+            <div class="navbarpad">
+                <a href="home.html">Home</a>
+                <div class="dropdown">
+                <button class="dropbtn">Tutorials &nbsp<i class="arrow-down"></i> </button>
+                <div class="dropdown-content">
+                    <a href="oop.html"> OOP </a>
+                    <a href="arrays.html"> Arrays </a>
+                    <a href="arraylists.html"> Arraylists </a>
+                    <a href="searchingsorting.html"> Searching & Sorting </a>
+                    <a href="recursion.html"> Recursion </a>
+                </div>
+                </div> 
+                <a href="addtionallearning.html">Projects</a>
+                <a href="bibliography.html">Bibliography</a>
+                <a href="aboutme.html">About me</a>
+            </div>
+        </div>`
+    );
+}
+
+var head = document.getElementsByTagName('HEAD')[0];
+var link = document.createElement('link');
+ 
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'navbar.css';
+ 
+head.appendChild(link);
+document.write(navbar());
