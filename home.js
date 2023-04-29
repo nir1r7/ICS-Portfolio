b94e380cce79b1fe87b4326f409babeccb3cdb1d
Nirvan Rabbani <nir1r7@gmail.com>
2023-04-28 20:14:16 -0400

    even more content

--- home.js
+++ home.js
@@ -1,32 +1,113 @@
+var points = [],
+    velocity2 = 2, // velocity squared
+    canvas = 
+document.getElementById('container'),
+  context = canvas.getContext('2d'),
+  radius = 8,
+  boundaryX = 400,
+  boundaryY = 400,
+  numberOfPoints = 25;
 
-let text = ["Developer", "Student", "Athlete", "Brother", ""];
+init();
 
-let i = 0;
+function init() {
+  // create points
+  for (var i = 0; i<numberOfPoints; i++) {
+    createPoint();
+  }
+  // create connections
+  for (var i = 0, l=points.length; i<l; i++) {
+    var point = points[i];
+    if(i == 0) {
+      points[i].buddy = points[points.length-1];
+    } else {
+      points[i].buddy = points[i-1];
+    }
+  }
+  
+  // animate
+  animate();
+}
 
-setInterval( () => {
-    // document.getElementById("title").innerHTML = text[i%4];
+function createPoint() {
+  var point = {}, vx2, vy2;
+  point.x = Math.random()*boundaryX;
+  point.y = Math.random()*boundaryY;
+  // random vx 
+  point.vx = (Math.floor(Math.random())*2-1)*Math.random();
+  vx2 = Math.pow(point.vx, 2);
+  // vy^2 = velocity^2 - vx^2
+  vy2 = velocity2 - vx2;
+  point.vy = Math.sqrt(vy2) * (Math.random()*2-1);
+  points.push(point);
+}
 
-    let chars = text[i%5].split('');
+function resetVelocity(point, axis, dir) {
+  var vx, vy;
+  if(axis == 'x') {
+    point.vx = dir*Math.random();  
+    vx2 = Math.pow(point.vx, 2);
+  // vy^2 = velocity^2 - vx^2
+  vy2 = velocity2 - vx2;
+  point.vy = Math.sqrt(vy2) * (Math.random()*2-1);
+  } else {
+    point.vy = dir*Math.random();  
+    vy2 = Math.pow(point.vy, 2);
+  // vy^2 = velocity^2 - vx^2
+  vx2 = velocity2 - vy2;
+  point.vx = Math.sqrt(vx2) * (Math.random()*2-1);
+  }
+}
 
-    displayWord(chars);
+function drawCircle(x, y) {
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  
+  let grd = context.createLinearGradient(0, 0, 0, 400);
+  grd.addColorStop(0, "#1e5c9e");
+  grd.addColorStop(1, "white");
 
-    i++;
-    document.getElementById("title").innerHTML = '';
+  context.fillStyle = grd;
+  context.fill();  
+}
 
-},800);
+function drawLine(x1, y1, x2, y2) {
 
-function displayWord(word){
+  let grd = context.createLinearGradient(0, 0, 0, 400);
+  grd.addColorStop(0, "#1e5c9e");
+  grd.addColorStop(1, "rgb(255,255,255,0.5");
 
-    let str = '';
-    word.forEach((letter, j) => (str += letter));
-    console.log(str);
+  context.beginPath();
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.strokeStyle = grd;
+  context.stroke(); /* #1e5c9ed3  #1e5c9e */
+}  
 
-    word.forEach((letter, i) =>
-        setTimeout(() => (document.getElementById("title").innerHTML += letter), 95 * i)
-    );
+function draw() {
+  for(var i =0, l=points.length; i<l; i++) {
+    // circles
+    var point = points[i];
+    point.x += point.vx;
+    point.y += point.vy;
+    drawCircle(point.x, point.y);
+    // lines
+    drawLine(point.x, point.y, point.buddy.x, point.buddy.y);
+    // check for edge
+    if(point.x < 0+radius) {
+      resetVelocity(point, 'x', 1);
+    } else if(point.x > boundaryX-radius) {
+      resetVelocity(point, 'x', -1);
+    } else if(point.y < 0+radius) {
+      resetVelocity(point, 'y', 1);
+    } else if(point.y > boundaryY-radius) {
+      resetVelocity(point, 'y', -1);
+    } 
+  }
+}
 
-    // for (let i = word.length; i > 0; i--){
-    //     setTimeout(() => (document.getElementById("title").innerHTML = str.substring(0, i)), 95 * i);
-    // }
-
+function animate() {
+  context.clearRect (0, 0, 400, 400);
+  draw();
+  requestAnimationFrame(animate);
 }
